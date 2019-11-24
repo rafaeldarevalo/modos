670d6837657e4c75a9a4984412e74a8916f846d3
Rafael Arevalo <rafaeldarevalo@gmail.com>
2019-11-23 23:27:39 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+// var myObj, i, j, x = "";
+// myObj = {
+//   "car1": [
+//     {"name":"Ford", "models":["Fiesta", "Focus", "Mustang"]},
+//   ]
+// };
+// for (i in myObj.car1) {
+//   x += "<h2>" + myObj.car1[i].name + "</h2>";
+//   for (j in myObj.car1[i].models) {
+//     x += myObj.car1[i].models[j] + "<br>";
+//   }
+// }
+
+
+// let signum = document.querySelectorAll('.signum93');
+// signum.innerHTML= x;
+// var i;
+// for (i = 0; i < signum.length; i++) {
+//     signum[i].innerHTML = x;
+// }
+// console.log(signum);
