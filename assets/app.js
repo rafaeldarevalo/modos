7c6d2308ea6f26be92b74d39ac5936f6fb68ba83
Rafael Arevalo <rafaeldarevalo@gmail.com>
2019-11-24 21:24:29 -0500

    lazy

--- assets/app.js
+++ assets/app.js
@@ -30,3 +30,30 @@ for (var i = 0; i < revealElements.length; i++) {
         .addIndicators({ name: "anima " + (i + 1) })
         .addTo(controller);
 }
+
+//CARGA DE IMÁGENES
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyImages = [].slice.call(document.querySelectorAll("img.js-lazy"));
+  
+    if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.srcset = lazyImage.dataset.srcset;
+            lazyImage.classList.remove("lazy");
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+  
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+      // Possibly fall back to a more compatible method here
+    }
+  });
+  
