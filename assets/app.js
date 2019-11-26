9366038a1a96156586b13540e588d27fdc0e9009
Rafael Arevalo <rafaeldarevalo@gmail.com>
2019-11-25 23:04:03 -0500

    otras secciones

--- assets/app.js
+++ assets/app.js
@@ -21,12 +21,29 @@
 // }
 // console.log(signum);
 var revealElements = document.getElementsByClassName("anima");
+var revealElementsHorizontal = document.getElementsByClassName("animaHorizontal");
 for (var i = 0; i < revealElements.length; i++) { 
     new ScrollMagic.Scene({
         triggerElement: revealElements[i], 
-        offset: 20,												
+        triggerElementHorizontal: revealElementsHorizontal[i], 
+        offset: -300,												
     })
+
+    
         .setClassToggle(revealElements[i], "visible")
+        .setClassToggle(revealElementsHorizontal[i], "visible")
         .addIndicators({ name: "anima " + (i + 1) })
         .addTo(controller);
 }
+
+/* -------------------------------------------------------------------------- */
+/*                                  PARALLAX                                  */
+/* -------------------------------------------------------------------------- */
+
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image, {
+    delay: 0.6,
+    transition: 'cubic-bezier(0,0,0,1)',
+    orientation: 'down',
+    
+});
